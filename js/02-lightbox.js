d5d1ecfd381147ef2b0b5ebed647eae7d8b2b032
Sergii Kurdybakha <103501357+s-kurdybakha@users.noreply.github.com>
2022-08-29 23:48:08 +0300

    done task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,73 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+
+const galleryContainer = document.querySelector('.gallery');
+const imagesGallery = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', imagesGallery);
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+  <a class="gallery__item" 
+    href="${original}">
+    <img class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+    />
+  </a>
+`;
+}).join('');
+}
+ 
+
+/* Реалізація делегування на div.gallery і отримання url великого зображення.*/
+
+
+function onGalleryContainerClick(evt) {
+    evt.preventDefault();
+
+    const isGalleryPhotoEl = evt.target.classList.contains('gallery__image');
+    
+    if (!isGalleryPhotoEl) {
+       return;
+        
+    }
+/* Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.*/
+    const PhotoUrlEl = evt.target.dataset.source;
+
+/* Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. 
+Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
+     */
+     
+    const instance = basicLightbox.create(`<img class="modal__image" src="${PhotoUrlEl}"/>`);
+    instance.show();
+
+    window.addEventListener('keydown', onEscKeyPress); 
+
+/* Закриття модалки по ESC */
+
+function onEscKeyPress(evt) {
+    const ESC_KEY_CODE = 'Escape';
+     
+    if (evt.code === ESC_KEY_CODE) {
+       instance.close();
+      
+        window.removeEventListener('keydown', onEscKeyPress); 
+    }
+}
+}
+
+// Подивися в документації секцію «Options» і додай відображення підписів до зображень з атрибута alt. Нехай підпис буде знизу і з'являється через 250 мілісекунд після відкриття зображення.
+
+var lightbox = new SimpleLightboooox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionPosition: 'bottom',
+    
+});
+
+
 console.log(galleryItems);
