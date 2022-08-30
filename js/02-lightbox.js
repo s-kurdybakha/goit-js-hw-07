dc94fcbe268542f042bc962e2043b2d8a95171a9
Sergii Kurdybakha <103501357+s-kurdybakha@users.noreply.github.com>
2022-08-30 21:33:43 +0300

    edit task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,7 +6,7 @@ import { galleryItems } from './gallery-items.js';
 const galleryContainer = document.querySelector('.gallery');
 const imagesGallery = createGalleryMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML('beforeend', imagesGallery);
-galleryContainer.addEventListener('click', onGalleryContainerClick);
+// galleryContainer.addEventListener('click', onGalleryContainerClick);
 
 function createGalleryMarkup(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
@@ -20,44 +20,6 @@ function createGalleryMarkup(galleryItems) {
   </a>
 `;
 }).join('');
-}
- 
-
-/* Реалізація делегування на div.gallery і отримання url великого зображення.*/
-
-
-function onGalleryContainerClick(evt) {
-    evt.preventDefault();
-
-    const isGalleryPhotoEl = evt.target.classList.contains('gallery__image');
-    
-    if (!isGalleryPhotoEl) {
-       return;
-        
-    }
-/* Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.*/
-    const PhotoUrlEl = evt.target.dataset.source;
-
-/* Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. 
-Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
-     */
-     
-    const instance = basicLightbox.create(`<img class="modal__image" src="${PhotoUrlEl}"/>`);
-    instance.show();
-
-    window.addEventListener('keydown', onEscKeyPress); 
-
-/* Закриття модалки по ESC */
-
-function onEscKeyPress(evt) {
-    const ESC_KEY_CODE = 'Escape';
-     
-    if (evt.code === ESC_KEY_CODE) {
-       instance.close();
-      
-        window.removeEventListener('keydown', onEscKeyPress); 
-    }
-}
 }
 
 // Подивися в документації секцію «Options» і додай відображення підписів до зображень з атрибута alt. Нехай підпис буде знизу і з'являється через 250 мілісекунд після відкриття зображення.
